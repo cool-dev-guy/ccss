ad93457056a4def9299a9f49e0c4e3ce989d8482
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2023-12-03 19:01:46 +0530

    Update ccss.js

--- src/v1/ccss.js
+++ src/v1/ccss.js
@@ -1,4 +1,4 @@
-const root = document.body;
+const root = document.getElementsByTagName("BODY")[0];
 const ccss = window.getComputedStyle(root, null);
 let ccss_render = ccss.getPropertyValue("--html");
 document.write(ccss_render);
